14ad81aa4235935e20b4bb6a2729282c016cf283
Matus Ferech <matus.ferech@telekom.com>
2017-10-11 00:59:17 +0100

    Add threading

--- glasgow_wifi.py
+++ glasgow_wifi.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 from threading import Thread
 from folium import Map, Circle, FeatureGroup, LayerControl
 
 
 def load_data(file):
-    data = []
+    protection_group = defaultdict(list)
     with open(file, 'r') as f:
         f.readline()
         for line in f:
-            Wifi = namedtuple('wifi', ['bssid', 'lat', 'long', 'protection'])
-            data.append(Wifi(*line.strip().split(',')))
-    return data
+            Wifi = namedtuple('wifi', ['lat', 'long', 'protection'])
+            _, lat, long, prot = line.strip().split(',')
+            protection_group[prot].append(Wifi(float(lat), float(long), prot))
+    return protection_group
+
+
+def add_markers(wifi_connections, protections):
+    for connection in wifi_connections:
+        Circle(
+            location=[connection.lat, connection.long],
+            radius=20,
+            color=colours[connection.protection],
+            fill=True,
+            fill_color=colours[connection.protection],
+            weight=1
+        ).add_to(protections[connection.protection])
+
+
+def add_feature_groups(groups, map):
+    for key, group in groups.items():
+        group.add_to(map)
 
 
 glasgow = Map(location=[55.873543, -4.289058],
@@ -23,8 +41,8 @@ glasgow = Map(location=[55.873543, -4.289058],
 colours = {
     'None': '#3186cc',
     'WPA': '#169316',
-    'WPA2': '#862D43',
-    'WEP': '#B9BF40'
+    'WPA2': '#963D53',
+    'WEP': '#A9AF30'
 }
 
 protections = {
@@ -34,21 +52,19 @@ protections = {
     'WEP': FeatureGroup(name='WEP')
 }
 
-wifi_connections = load_data('glasgow-dl.csv')
+wifi_connections = load_data('test.csv')
+threads = [Thread(target=add_markers, args=[wifi_connections[conn_type], protections])
+                for conn_type in wifi_connections]
 
-for connection in wifi_connections:
-    Circle(
-        location=[float(connection.lat), float(connection.long)],
-        radius=20,
-        color=colours[connection.protection],
-        fill=True,
-        fill_color=colours[connection.protection],
-        weight=1
-    ).add_to(protections[connection.protection])
+[t.start() for t in threads]
+[t.join() for t in threads]
 
-for key, group in protections.items():
-    group.add_to(glasgow)
-
+add_feature_groups(protections, glasgow)
 LayerControl().add_to(glasgow)
-glasgow.save('glasgow.html')
-#  https://data.glasgow.gov.uk/dataset/wi-fi-access-point-locations
+
+glasgow.save('glasgow2.html')
+
+# SOURCE OF DATASET
+# https://data.glasgow.gov.uk/dataset/wi-fi-access-point-locations
+# LICENCE OF DATASET
+# http://open.glasgow.gov.uk/ckansupport/open.glasgow.gov.uk_Licence_version 1.0.htm
